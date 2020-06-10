c9bd55bf69ae76d57c2d87d51c8ba4f689b93fc5
Leo Estela <leoestela@hotmail.com>
2020-06-10 02:04:32 +0200

    UserService: Actualize user logic and tests

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -3,27 +3,67 @@
 
 namespace App\Service;
 
-
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Exception;
 
 class UserService
 {
+    /** @var ManagerRegistry */
+    private $managerRegistry;
+
+    /** @var UserRepository */
+    private $userRepository;
+
+
+    public function __construct(ManagerRegistry $managerRegistry, UserRepository $userRepository)
+    {
+        $this->managerRegistry = $managerRegistry;
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * @param string $email
      * @param string $phone
      * @param string $address
+     * @return User
      * @throws Exception
      */
-    public function actualizeUser(string $email, string $phone, string $address): void
+    public function actualizeUser(string $email, string $phone, string $address): User
     {
-        if (!$this->allRequiredFieldsArePresent($email, $phone, $address))
-        {
-            throw new Exception('Required field not informed', 100);
-        }
+        $this->requiredFieldInformed($email);
+        $this->requiredFieldInformed($phone);
+        $this->requiredFieldInformed($address);
 
         if (!$this->isValidEmail($email))
         {
             throw new Exception('Invalid e-mail', 100);
+        }
+
+        $user = $this->userRepository->findOneByEmail($email);
+
+        if (null == $user)
+        {
+            $user = $this->createUser($email, $phone, $address);
+        }
+        else 
+        {
+            $user = $this->modifyUser($user, $email, $phone, $address);
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param string $requiredField
+     * @throws Exception
+     */
+    private function requiredFieldInformed(string $requiredField)
+    {
+        if (null == $requiredField)
+        {
+            throw new Exception('Required field not informed', 100);
         }
     }
 
@@ -42,5 +82,32 @@ class UserService
     private function isValidEmail(string $email):bool
     {
         return (false !== filter_var($email, FILTER_VALIDATE_EMAIL));
+    }
+
+    private function saveUser(User $user)
+    {
+        $entityManager = $this->managerRegistry->getManagerForClass('App\Entity\User');
+        $entityManager->persist($user);
+        $entityManager->flush();
+    }
+
+    private function createUser(string $email, string $phone, string $address): User
+    {
+        $user = new User($email, $phone, $address);
+
+        $this->saveUser($user);
+
+        return $user;
+    }
+
+    private function modifyUser(User $user, string $email, string $phone, string $address): User
+    {
+        $user->setEmail($email);
+        $user->setPhone($phone);
+        $user->setAddress($address);
+
+        $this->saveUser($user);
+
+        return $user;
     }
 }
--- tests/Unit/Service/UserServiceTest.php
+++ tests/Unit/Service/UserServiceTest.php
@@ -2,9 +2,15 @@
 
 namespace App\Tests\Unit\Service;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Service\UserService;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
 use Exception;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class UserServiceTest extends TestCase
 {
@@ -15,9 +21,47 @@ class UserServiceTest extends TestCase
     /** @var UserService */
     private $userService;
 
+    /**  @var ObjectProphecy|User */
+    private $userProphecy;
+
+    /** @var User */
+    private $user;
+
+    /**  @var ObjectProphecy|ManagerRegistry */
+    private $managerRegistryProphecy;
+
+    /** @var ManagerRegistry */
+    private $managerRegistry;
+
+    /**  @var ObjectProphecy|ObjectManager */
+    private $entityManagerProphecy;
+
+    /** @var ObjectManager */
+    private $entityManager;
+
+    /** @var ObjectProphecy|UserRepository */
+    private $userRepositoryProphecy;
+
+    /** @var UserRepository */
+    private $userRepository;
+
+
     public function setUp()
     {
-        $this->userService = new UserService();
+        $this->userProphecy = $this->prophesize(User::class);
+        $this->user = $this->userProphecy->reveal();
+
+        $this->managerRegistryProphecy = $this->prophesize(ManagerRegistry::class);
+        $this->managerRegistry = $this->managerRegistryProphecy->reveal();
+
+        $this->entityManagerProphecy = $this->prophesize(ObjectManager::class);
+        $this->entityManagerProphecy = $this->entityManagerProphecy->willBeConstructedWith([User::class]);
+        $this->entityManager = $this->entityManagerProphecy->reveal();
+
+        $this->userRepositoryProphecy = $this->prophesize(UserRepository::class);
+        $this->userRepository = $this->userRepositoryProphecy->reveal();
+
+        $this->userService = new UserService($this->managerRegistry, $this->userRepository);
     }
 
     public function aExceptionIsExpected()
@@ -51,6 +95,66 @@ class UserServiceTest extends TestCase
     {
         $this->aExceptionIsExpected();
         $this->userService->actualizeUser('leoestela@', self::PHONE,self::ADDRESS);
+    }
+
+    /** @throws */
+    public function testShouldModifyUserIfExists()
+    {
+        $this->userRepositoryProphecy
+            ->findOneByEmail(self::EMAIL)
+            ->shouldBeCalledOnce()
+            ->willReturn($this->userProphecy);
+        $this->userProphecy->setEmail(self::EMAIL)->shouldBeCalledOnce();
+        $this->userProphecy->setPhone(self::PHONE)->shouldBeCalledOnce();
+        $this->userProphecy->setAddress(self::ADDRESS)->shouldBeCalledOnce();
+
+        $this->managerRegistryProphecy
+            ->getManagerForClass('App\Entity\User')
+            ->shouldBeCalledOnce()
+            ->willReturn($this->entityManagerProphecy);
+
+        $this->entityManagerProphecy->persist($this->userProphecy)->shouldBeCalledOnce();
+        $this->entityManagerProphecy->flush()->shouldBeCalledOnce();
+
+        try {
+            $modifiedUser = $this->userService
+                ->actualizeUser(self::EMAIL,self::PHONE,self::ADDRESS);
+
+            $this->assertInstanceOf('App\Entity\User', $modifiedUser);
+
+        } catch (Exception $exception) {
+            $this->fail($exception->getMessage());
+        }
+    }
+
+    /** @throws */
+    public function testShouldCreatUserIfEmailNotExists()
+    {
+        $this->userRepositoryProphecy->findOneByEmail(self::EMAIL)->shouldBeCalledOnce()->willReturn(null);
+
+        $this->managerRegistryProphecy
+            ->getManagerForClass('App\Entity\User')
+            ->shouldBeCalledOnce()
+            ->willReturn($this->entityManagerProphecy);
+
+        $this->entityManagerProphecy->persist(
+            Argument::that(
+                function(User $user):bool
+                {
+                    return true;
+                }
+                ))->shouldBeCalledOnce();
+        $this->entityManagerProphecy->flush()->shouldBeCalledOnce();
+
+        try {
+            $modifiedUser = $this->userService
+                ->actualizeUser(self::EMAIL,self::PHONE,self::ADDRESS);
+
+            $this->assertInstanceOf('App\Entity\User', $modifiedUser);
+
+        } catch (Exception $exception) {
+            $this->fail($exception->getMessage());
+        }
     }
 }
 
