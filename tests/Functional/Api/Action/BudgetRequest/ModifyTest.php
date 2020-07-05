7e0137940993921ea457493ceb440276e6510cea
Leo Estela <leoestela@hotmail.com>
2020-07-05 20:41:47 +0200

    Functional tests for modify endpoint

---
+++ tests/Functional/Api/Action/BudgetRequest/ModifyTest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Tests\Functional\Api\Action\BudgetRequest;
+
+
+use App\Api\EndpointUri;
+use App\DataFixtures\DataFixtures;
+use App\Tests\Functional\FunctionalWebTestCase;
+use Exception;
+
+class ModifyTest extends FunctionalWebTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->loadFixtures();
+    }
+
+    public function testModifyBudgetRequest()
+    {
+        $payload = ['title' => DataFixtures::BUDGET_REQUEST_NEW_TITLE];
+
+        $client = static::createClient();
+        $client->request(
+            'PUT',
+            str_replace('{budgetRequestId}', '1',EndpointUri::URI_BUDGET_REQUEST_MODIFY),
+            [],
+            [],
+            [],
+            json_encode($payload),
+            true);
+
+        $this->assertEquals(201, $client->getResponse()->getStatusCode());
+    }
+
+    /** @throws Exception */
+    public function tearDown()
+    {
+        $this->purge();
+    }
+}
